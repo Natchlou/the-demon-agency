d94615d24b4ff3a222a0ef4672e97b2a7d597819
Natchlou <n.jullien57@gmail.com>
2025-04-01 00:20:32 +0200

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white px-4">
-      <h1 className="text-7xl font-extrabold text-red-700 drop-shadow-lg animate-bounce">
-        The Demon Agency
-      </h1>
-      <p className="text-2xl mt-4 text-center max-w-lg tracking-wide">
-        Du contenu <span className="text-red-700 font-semibold">enflammé</span>, une audience possédée !
-      </p>
-    </div>
+    <>
+      <SiteHeader title={'Page d\'accueil'} />
+      <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white px-4">
+        <h1 className="text-7xl font-extrabold text-red-700 drop-shadow-lg animate-bounce">
+          The Demon Agency
+        </h1>
+        <p className="text-2xl mt-4 text-center max-w-lg tracking-wide">
+          Du contenu <span className="text-red-700 font-semibold">enflammé</span>, une audience possédée !
+        </p>
+      </div>
+    </>
   );
 }
