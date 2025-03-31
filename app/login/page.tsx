1f97fe019abb85aca770978b9923e0e048c3d9eb
Natchlou <n.jullien57@gmail.com>
2025-03-31 14:53:15 +0200

    fix all problems

---
+++ app/login/page.tsx
@@ -0,0 +1,15 @@
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+)
+
+const App = () => (
+    <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+    />
+)
