c53010ce4694d05c713fbd7535639640abb09e5f
Natchlou <n.jullien57@gmail.com>
2025-04-02 00:12:22 +0200

    remove actions to data table

--- components/list-match.tsx
+++ components/list-match.tsx
@@ -142,36 +142,7 @@ export const columns: CustomColumnDef<Match>[] = [
         cell: ({ row }) => (
             <div className="capitalize">{row.getValue("description")}</div>
         ),
-    },
-    {
-        id: "actions",
-        enableHiding: false,
-        cell: ({ row }) => {
-            const payment = row.original
-
-            return (
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                            onClick={() => navigator.clipboard.writeText(payment.id)}
-                        >
-                            Copy payment ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View customer</DropdownMenuItem>
-                        <DropdownMenuItem>View payment details</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            )
-        },
-    },
+    }
 ]
 
 export function DataTableDemo({ data, isAdmin }: { data: Match[], isAdmin: boolean }) {
